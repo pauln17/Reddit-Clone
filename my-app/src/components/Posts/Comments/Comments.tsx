518dfcde092687db86f7a3a69307ea1f872e6e10
Paul <paulnguyen0417@gmail.com>
2023-11-20 21:40:14 -0500

    Deleting Comments Functionality

--- my-app/src/components/Posts/Comments/Comments.tsx
+++ my-app/src/components/Posts/Comments/Comments.tsx
@@ -23,6 +23,7 @@ const Comments: React.FC<CommentsProps> = ({
     const [comments, setComments] = useState<Comment[]>([])
     const [fetchLoading, setFetchLoading] = useState(true)
     const [createLoading, setCreateLoading] = useState(false)
+    const [loadingDeleteId, setLoadingDeleteId] = useState('');
     const setPostState = useSetRecoilState(postState);
 
     const onCreateComment = async (commentText: string) => {
@@ -75,11 +76,37 @@ const Comments: React.FC<CommentsProps> = ({
         setCreateLoading(false);
     }
 
-    const onDeleteComment = async (comment: any) => {
-        // delete the comment document
-        // update post numberOfComments - 1
+    const onDeleteComment = async (comment: Comment) => {
+        setLoadingDeleteId(comment.id as string);
+        try {
+            if (!comment.id) throw "Comment has no ID";
+            const batch = writeBatch(firestore);
+            // delete the comment document
+            const commentDocRef = doc(firestore, "comments", comment.id);
+            batch.delete(commentDocRef);
 
-        // update client recoil state
+            // update post numberOfComments - 1
+            const postDocRef = doc(firestore, 'posts', selectedPost?.id!);
+            batch.update(postDocRef, {
+                numberOfComments: increment(-1),
+            })
+
+            await batch.commit();
+
+            // update client recoil state
+            setPostState((prev) => ({
+                ...prev,
+                selectedPost: {
+                    ...prev.selectedPost,
+                    numberOfComments: prev.selectedPost?.numberOfComments! - 1
+                } as Post
+            }))
+
+            setComments(prev => prev.filter(item => item.id !== comment.id));
+        } catch (error) {
+            console.log("onDeleteComment error", error)
+        }
+        setLoadingDeleteId('');
     }
 
     const getPostComments = async () => {
@@ -157,12 +184,12 @@ const Comments: React.FC<CommentsProps> = ({
                         ) : (
                             <>
                                 {
-                                    comments.map((item: Comment) => (
+                                    comments.map((comment) => (
                                         <CommentItem
-                                            key={item.id}
-                                            comment={item}
+                                            key={comment.id}
+                                            comment={comment}
                                             onDeleteComment={onDeleteComment}
-                                            loadingDelete={false}
+                                            loadingDelete={loadingDeleteId === comment.id}
                                             userId={user.uid}
                                         />
                                     ))
